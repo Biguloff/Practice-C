4fa1ed7b405357ba8f8a1b28a95b9cb9ca621575
Sergey Bigulov <biguloff@gmail.com>
2015-05-14 02:44:36 +0300

    ex14.c err wtf

--- ex14.c
+++ ex14.c
@@ -3,40 +3,40 @@
 
 
 int can_print_it(char ch);
-void print_letters(char arg[]);
+void print_letters(char argv[]);
 
-void print_arguments(int argc,char  *argv[])
+void print_arguments(int argc,char *argv[])
 {
 	int i = 0;
 
 
-	for (i=0;i<argc;i++)
-		{
-			print_letters(argv[i]);
-		}
+	for(i = 0;i<argc;i++)
+	{		printf("agv i %c\n",*argv[i] );
+			printf("arg %d\n ", i);
+		print_letters(argv[i]);	
+	}
 }
 
-
-void print_letters(char arg[])
+void print_letters(char argv[])
 {
-    int i = 0;
+	int i = 0;
 
-    for(i = 0;arg[i] != '\0'; i++) 
-    {
-        char ch = arg[i];
-
-        if(can_print_it(ch)) 
-        {
-            printf("'%c' == %d ",ch,ch);
-        }
-    }
-
-    printf("\n");
+	for(i=0;argv[i] != '\0'; i++);
+	{
+		printf("%c\n",argv[i] );
+		char ch = argv[i];
+				printf("let %d\n ch %c", i, ch);
+		if(can_print_it(ch))
+		{
+						printf("arg %d\n ", i);
+			printf("'%c' == %d ",ch,ch);
+		}
+	}
+printf("\n");
 }
 
 int can_print_it(char ch)
-{
-	return isalpha(ch) || isblank(ch);
+{	return isalpha(ch) || isblank(ch);
 }
 
 int main(int argc, char *argv[])
@@ -44,3 +44,4 @@ int main(int argc, char *argv[])
 	print_arguments(argc,argv);
 	return 0;
 }
+
